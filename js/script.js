de44f45ad4df5b255854e18693a25d4958ebcbef
bidduthosen <mdbidduthosen34@gmail.com>
2022-09-03 08:20:17 +0600

    modal add news category section

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ const loadSomoyTvNews = async() => {
 };
 
 const displaySomoyTvNews = (newsfied) => {
-    // console.log(newsfied);
     const somoyTvNews = document.getElementById('somoy-tv-news');
     newsfied.forEach(news => {
         const somoydiv = document.createElement('div');
@@ -30,10 +29,10 @@ const displayNewsDitails = (categoryDitails) => {
     const newsDitails = document.getElementById('news-ditails');
     newsDitails.textContent = ``;
     categoryDitails.forEach(category =>{
-        console.log(category)
+        // console.log(category)
         const categoryDiv = document.createElement('div');
         categoryDiv.innerHTML = `
-            <div class="card mb-5">
+            <div onclick="loadNewsModal('${category._id}')" data-bs-toggle="modal" data-bs-target="#staticBackdrop" class="card mb-5">
             <div class="row g-0">
                 <div class="col-md-4">
                 <img src="${category.thumbnail_url}" class="img-fluid rounded-start" alt="...">
@@ -45,17 +44,17 @@ const displayNewsDitails = (categoryDitails) => {
                     <footer class="d-md-flex flex-wrap justify-content-between align-items-center py-3 my-4">
                       <div class="col-sm-12 col-md-2 d-md-flex align-items-center">
                         <a href="#" class="mb-3 me-2 mb-md-0 text-decoration-none">
-                        <img class="h-50 w-50 rounded-5" src="${category.author.img}" alt="">
+                        <img class="h-50 w-50 rounded-5" src="${category.author.img ? category.author.img : 'No data found'}" alt="">
                         </a>
                       </div>
                       <div class="col-sm-12 col-md-4 text-md-start">
-                        <h6>${category.author.name}</h6> 
-                        <h6>${category.author.published_date}</h6>
+                        <h6>${category.author.name ? category.author.name : 'No data found'}</h6> 
+                        <h6>${category.author ? category.author.published_date : 'No data found'}</h6>
                       </div>
                       <ul class="nav col-sm-12 col-md-3 justify-content-md-center d-md-flex">
                       
                         <li class="ms-3"><i class="fa-solid fa-eye"></i></li>
-                        <li class="ms-3">${category.rating.number}M</li>
+                        <li class="ms-3">${category.rating ? category.rating.number : 'No data found'}M</li>
                       </ul>
                       <ul class="nav col-sm col-md-3 justify-content-lg-end d-md-flex">
                       <button type="button" class="btn btn-info"><i class="fa-solid fa-arrow-right"></i></button>
@@ -71,5 +70,30 @@ const displayNewsDitails = (categoryDitails) => {
 
     });
 };
+
+const loadNewsModal = async(id) => {
+    const url = `https://openapi.programming-hero.com/api/news/${id}`;
+    const response = await fetch(url)
+    const data = await response.json()
+    displayNewsModal(data.data[0])
+};
+
+const displayNewsModal = newsModal => {
+    // console.log(newsModal._id)
+    const newsDitailsModal = document.getElementById('xLabel');
+    newsDitailsModal.innerText = newsModal._id;
+    // const newsModal =document.getElementById('news-modal');
+    // newsModal.innerHTML =`
+    // <img class="d-flex m-auto" src='${phone.image}'>
+    // <div class="text-center">
+    //     <P class = "mt-5">ChipSet: ${phone.mainFeatures.chipSet}</p>
+    //     <P>DisplaySize: ${phone.mainFeatures.displaySize}</p>
+    //     <P>Memory: ${phone.mainFeatures ? phone.mainFeatures.memory : 'not storage'}</p>
+    //     <P>ReleaseDate: ${phone.releaseDate ? phone.releaseDate : 'Upcomming...'}</p>
+    // </div>
+    // `;
+
+}
+
 
 loadSomoyTvNews()
