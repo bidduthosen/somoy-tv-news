e4e1352470a8dbe9643b743092a0c3efe04aeac6
bidduthosen <mdbidduthosen34@gmail.com>
2022-09-03 23:49:28 +0600

    category length not found condition add

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ const displaySomoyTvNews = (newsfied) => {
         const somoydiv = document.createElement('div');
         somoydiv.innerHTML = `
         <ul onclick= "loadNewsDitails('${news.category_id}')">
-            <li class= "li-bg-hover rounded px-2">${news.category_name}</li>
+            <li class= "li-bg-hover rounded px-4 py-1">${news.category_name}</li>
         </ul>
         `;
         somoyTvNews.appendChild(somoydiv);
@@ -38,49 +38,55 @@ const loadNewsDitails = async (categoryId) =>{
 };
 
 
-// loadNewsDitails('01');
-
 const displayNewsDitails = (categoryDitails) => {
     const newsDitails = document.getElementById('news-ditails');
     newsDitails.textContent = ``;
-    categoryDitails.forEach(category =>{
-        // console.log(category)
-        const categoryDiv = document.createElement('div');
-        categoryDiv.innerHTML = `
-            <div onclick="loadNewsModal('${category._id}')" data-bs-toggle="modal" data-bs-target="#newsDitailsModal" class="card mb-5">
-            <div class="row g-0">
-                <div class="col-md-4">
-                <img src="${category.thumbnail_url}" class="img-fluid rounded-start" alt="...">
-                </div>
-                <div class="col-md-8">
-                <div class="card-body">
-                <h5 class="card-title">${category.title}</h5>
-                <p class="card-text">${category.details.slice(0, 400)}</p>
-                    <footer class="d-md-flex flex-wrap justify-content-between align-items-center py-3 my-4">
-                      <div class="col-sm-12 col-md-2 d-md-flex align-items-center">
-                        <a href="#" class="mb-3 me-2 mb-md-0 text-decoration-none">
-                        <img class="h-50 w-50 rounded-5" src="${category.author.img ? category.author.img : 'No data found'}" alt="">
-                        </a>
-                      </div>
-                      <div class="col-sm-12 col-md-4 text-md-start">
-                        <h6>${category.author.name ? category.author.name : 'No data found'}</h6> 
-                        <h6>${category.author.published_date ? category.author.published_date : 'No data found'}</h6>
-                      </div>
-                      <ul class="nav col-sm-12 col-md-3 justify-content-md-center d-md-flex">
-                      
-                        <li class="ms-3"><i class="fa-solid fa-eye"></i></li>
-                        <li class="ms-3">${category.total_view ? category.total_view  : 'No data found' }</li>
-                      </ul>
-                      <ul class="nav col-sm col-md-3 justify-content-lg-end d-md-flex">
-                      <button type="button" class="btn btn-info"><i class="fa-solid fa-arrow-right"></i></button>
-                      </ul>
-                    </footer>
+    if(categoryDitails.length == 0){
+        document.getElementById('not-found').innerHTML =`<h3 class="bg-success bg-opacity-25 rounded ps-4 py-3" >Not Found</h3>`;
+    }
+    else{
+        document.getElementById('not-found').innerHTML =`<h3 class="bg-success bg-opacity-25 rounded ps-4 py-3">${categoryDitails.length} Items Found</h3>`;
+        categoryDitails.forEach(category =>{
+            // console.log(category)
+            const categoryDiv = document.createElement('div');
+            categoryDiv.innerHTML = `
+                <div onclick="loadNewsModal('${category._id}')" data-bs-toggle="modal" data-bs-target="#newsDitailsModal" class="card mb-5">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="${category.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                    <h5 class="card-title">${category.title}</h5>
+                    <p class="card-text">${category.details.slice(0, 400)}</p>
+                        <footer class="d-md-flex flex-wrap justify-content-between align-items-center py-3 my-4">
+                          <div class="col-sm-12 col-md-2 d-md-flex align-items-center">
+                            <a href="#" class="mb-3 me-2 mb-md-0 text-decoration-none">
+                            <img class="h-50 w-50 rounded-5" src="${category.author.img ? category.author.img : 'No data found'}" alt="">
+                            </a>
+                          </div>
+                          <div class="col-sm-12 col-md-4 text-md-start">
+                            <h6>${category.author.name ? category.author.name : 'No data found'}</h6> 
+                            <h6>${category.author.published_date ? category.author.published_date : 'No data found'}</h6>
+                          </div>
+                          <ul class="nav col-sm-12 col-md-3 justify-content-md-center d-md-flex">
+                          
+                            <li class="ms-3"><i class="fa-solid fa-eye"></i></li>
+                            <li class="ms-3">${category.total_view ? category.total_view  : 'No data found' }</li>
+                          </ul>
+                          <ul class="nav col-sm col-md-3 justify-content-lg-end d-md-flex">
+                          <button type="button" class="btn btn-info"><i class="fa-solid fa-arrow-right"></i></button>
+                          </ul>
+                        </footer>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div> `;
-        newsDitails.appendChild(categoryDiv);
-    });
+            </div> `;
+            newsDitails.appendChild(categoryDiv);
+        });
+
+    }
+    
     // toggleSpinners stop
     toggleSpinners(false);
 };
@@ -128,5 +134,6 @@ const displayNewsModal = newsModal => {
 
 }
 
+loadNewsDitails('05');
 
-loadSomoyTvNews()
+loadSomoyTvNews();
